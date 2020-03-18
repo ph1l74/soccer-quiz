a602600e0d49a43f021782b3e6abf4d8376f6829
Filat Astakhov <astakhovfilat@gmail.com>
2020-03-18 15:16:50 +0500

    Изменены стили, добавлен favicon

--- src-frontend/src/Copmonents/MainContainer.js
+++ src-frontend/src/Copmonents/MainContainer.js
@@ -6,7 +6,9 @@ import axios from 'axios';
 
 
 function MainContainer() {
-    const getQuestions = useSelector(state => state );
+    const getQuestions = useSelector(state => state.questions);
+    const getAnswers = useSelector(state => state.answers);
+    const curQuestion = useSelector(state => state.curQuestion);
     const isLoading = useSelector(state => state.isLoading);
     const dispatch = useDispatch();
 
@@ -15,7 +17,7 @@ function MainContainer() {
 
         const fetchData = async () => {
             dispatch(startLoading());
-            const res = await axios("http://localhost:80/api/coef");
+            const res = await axios("/api/coef");
             dispatch(addData(res.data));
             dispatch(stopLoading());
         };
@@ -48,14 +50,36 @@ function MainContainer() {
                     <div className="hero-body">
                         <div id="contentArea" className="container has-text-centered">
                             <div className="columns is-centered">
-                                {getQuestions.questions && getQuestions.questions.length > 0 ? (
-                                    <div
-                                        className="column is-one-column-mobile is-two-thirds-tablet is-half-desktop is-half-widescreen is-half-fullhd">
-                                        {getQuestions.questions.map((q, i) => {
-                                            return <Card questionData={q} key={`card_${i}`} num={i}></Card>
-                                        })}
-                                    </div>)
-                                    : (<div></div>)}
+                                {
+                                    getQuestions && getQuestions.length > 0 ? (
+                                        <div
+                                            className="column is-one-column-mobile is-two-thirds-tablet is-half-desktop is-half-widescreen is-half-fullhd">
+                                            {getQuestions.map((q, i) => {
+                                                return <Card questionData={q} key={`card_${i}`} num={i}></Card>
+                                            })}
+                                            {
+                                                curQuestion === getQuestions.length ?
+                                                    (
+                                                        <div className="sq-results">
+                                                            <div className="sq-results-header is-size-3">Молодец!</div>
+                                                            <div className="sq-result-text is-size-5"></div>
+                                                            <div className="sq-result-answers is-size-5">Твои ответы:</div>
+                                                            {getAnswers.map((a, i) => (
+                                                                <div className="sq-result-answer is-size-4">
+                                                                    <div className="sq-result-answer-text">{a}</div>
+                                                                    <span>—</span>
+                                                                    <div className="sq-result-answer-coef">Коэф. {getQuestions[i].coef}</div>
+                                                                </div>
+                                                            ))}
+                                                            <div className="sq-result-aftertext is-size-5">Задепозить 1000, получишь бонус ещё 1000₽. В итоге поставив 1000₽ на победу Барселоны получишь 2300₽!</div>
+                                                            <a className="button is-rounded" href="http://ph1l74.com/">ЗАДЕПОЗИТИТЬ</a>
+                                                        </div>
+                                                    )
+                                                    : (null)
+                                            }
+                                        </div>)
+                                        : (null)
+                                }
                             </div>
                         </div>
                     </div>
